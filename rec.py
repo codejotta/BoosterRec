ba117d04ed88382e173dd22265c6ba6f6e3f2b33
JosueBelarmino <jottab.br@gmail.com>
2018-12-08 03:07:31 -0200

    system tray icon

--- rec.py
+++ rec.py
@@ -1,7 +1,8 @@
 import sys
-from PySide2.QtWidgets import QApplication, QMessageBox, QWidget, QPushButton, QLabel, QLineEdit, QHBoxLayout, QVBoxLayout, QFileDialog
+from PySide2.QtWidgets import (QApplication, QMessageBox, QWidget, QPushButton, QLabel, QLineEdit, QHBoxLayout, QVBoxLayout, QGridLayout, QFileDialog, QGroupBox, 
+								QSystemTrayIcon, QMenu, QAction)
 from PySide2.QtGui import QIcon, QPixmap, QImage
-from PySide2.QtCore import Qt
+from PySide2.QtCore import Qt, QCoreApplication
 from PIL import ImageQt, ImageGrab, Image
 
 class Window(QWidget):
@@ -12,8 +13,10 @@ class Window(QWidget):
 	def initUI(self):
 		folderLabel = QLabel("Pasta:")
 		folderLabel.setFixedWidth(30)
+
 		self.folderEntry = QLineEdit(sys.path[0]+"\\"+"screenshot.png", self)
 		self.folderEntry.setFixedWidth(300)
+
 		folderButton = QPushButton("Procurar...", self)
 		folderButton.setFixedWidth(75)
 		folderButton.clicked.connect(self.open_folder)
@@ -22,32 +25,40 @@ class Window(QWidget):
 		folderLayout.addWidget(self.folderEntry)
 		folderLayout.addWidget(folderButton)
 
-		self.screenFrame = QLabel(self)
+		box = QGroupBox("Última captura da tela")
+		boxLayout = QVBoxLayout()
+		self.screenFrame = QLabel(box)
 		self.capture()
+		boxLayout.addWidget(self.screenFrame)
+		box.setLayout(boxLayout)
 
 		captureButton = QPushButton("Capturar tela", self)
-		captureButton.setFixedWidth(100)
+		captureButton.setFixedWidth(85)
 		captureButton.clicked.connect(self.capture)
+
+		optionsButton = QPushButton("Opções", self)
+		optionsButton.setFixedWidth(85)
+
 		saveButton = QPushButton("Salvar Imagem", self)
-		saveButton.setFixedWidth(100)
+		saveButton.setFixedWidth(85)
 		saveButton.clicked.connect(self.save_image)
-		bottomLayout = QHBoxLayout()
-		bottomLayout.addWidget(captureButton)
-		bottomLayout.addWidget(saveButton)
-		bottomLayout.setSpacing(218)
-		bottomLayout.setAlignment(Qt.AlignCenter)
+
+		bottomLayout = QGridLayout()
+		bottomLayout.addWidget(captureButton,0,0)
+		bottomLayout.addWidget(optionsButton,0,1)
+		bottomLayout.addWidget(QLabel(),0,2)
+		bottomLayout.addWidget(saveButton,0,3)
 
 		self.layout = QVBoxLayout()
 		self.layout.addLayout(folderLayout)
-		self.layout.addWidget(self.screenFrame)
+		self.layout.addWidget(box)
 		self.layout.addLayout(bottomLayout)
 		self.layout.setAlignment(Qt.AlignLeft)
 		self.layout.setAlignment(Qt.AlignTop)
-		self.setLayout(self.layout)
 
+		self.setLayout(self.layout)
 		self.setWindowTitle('Booster Rec')
 		self.setWindowIcon(QIcon('icon.png'))
-		self.show()
 
 	def open_folder(self):
 		folder = QFileDialog.getSaveFileName(self, 'Selecionar pasta', '', "PNG (*.png);;JPEG (*.jpg);; Todos arquivos (*.*)")
@@ -75,7 +86,27 @@ class Window(QWidget):
 		img = QPixmap.fromImage(img)
 		self.screenFrame.setPixmap(img)
 
+	def closeEvent(self, event):
+		print("AD")
+		self.hide()
+		event.ignore()
+
+class SystemTrayIcon(QSystemTrayIcon):
+	def __init__(self, icon, parent=None):
+		QSystemTrayIcon.__init__(self, icon, parent)
+		menu = QMenu(parent)
+		exitAction = menu.addAction("Exit")
+		exitAction.triggered.connect(QCoreApplication.exit)
+		initAction = menu.addAction("Setup")
+		initAction.triggered.connect(self.initUI)
+		self.setContextMenu(menu)
+
+	def initUI(self):
+		win.show()
+
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
-	wi = Window()
+	trayIcon = SystemTrayIcon(QIcon("icon.png"))
+	trayIcon.show()
+	win = Window()
 	sys.exit(app.exec_())
